a8b0d97dfa31e4528f99976c119bb212974d7e0b
Alen-T-Alex <140389582+Alen-T-Alex@users.noreply.github.com>
2025-04-06 18:53:29 +0300

    Add files via upload

---
+++ daily_plsn.py
@@ -0,0 +1,4 @@
+activity=str(input("what activity is it today sir?"))
+time=float(input("number of hours preforming the task"))
+priority=input("how important is the task")
+print(f"good morning sir,today you have to do {activity} for {time} hours, priority={priority}")
